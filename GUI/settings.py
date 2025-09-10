79eabc7d51d549905a68b7c58b9537709193b232
rithika1208 <rithikarithu0810@gmail.com>
2025-09-10 15:28:06 +0530

    Fix template path for home.html

--- GUI/settings.py
+++ GUI/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = "GUI.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [ BASE_DIR /'templates'],
+        "DIRS": [ BASE_DIR /'basics'/'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
